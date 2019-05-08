8675d9d4ae58641de46380f82d32152afd9580ac
WitlessWonder <benplangdon@gmail.com>
2019-05-08 16:11:07 -0400

    updated unity

---
+++ Game/Assets/DirectionalField.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionalField : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Field Initialized");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Update");
+    }
+    public void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Entered Collision");
+    }
+    public void OnCollisionStay(Collision other)
+    {
+        Debug.Log("Stay");
+    }
+    public void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("Collision Exited");
+    }
+}
---
+++ Game/Assets/moveScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 1;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = 0;
+        float y = 0;
+        float z = 0;
+        float dT = Time.deltaTime;
+        if(Input.GetKey(KeyCode.W)){
+            x+=speed;
+        }
+        else if (Input.GetKey(KeyCode.S)){
+            x-=speed;
+        }
+        if(Input.GetKey(KeyCode.A)){
+            y+=speed;
+        }
+        else if (Input.GetKey(KeyCode.D)){
+            y-=speed;
+        }
+        if(Input.GetKey(KeyCode.Q)){
+            z+=speed;
+        }
+        else if (Input.GetKey(KeyCode.E)){
+            z-=speed;
+        }
+        Vector3 velocity = new Vector3(x,y,z);
+        this.transform.position += velocity*dT;
+
+    }
+    
+}
